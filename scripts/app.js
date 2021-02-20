e14dfb90ac981cff330384299a627f9b1a12474e
hamzaaMB <hamzambutt1@gmail.com>
2021-02-20 22:50:36 +0000

    controls working

--- scripts/app.js
+++ scripts/app.js
@@ -7,9 +7,9 @@ function init() {
   const cellCount = width * height
   const cells = []
 
-  let shipStartingPosition = 104
-  let shipCurrentPosition = 0
   const shipClass = 'ship'
+  let shipStartingPosition = 104
+  let shipCurrentPosition = 104
   
 
 
@@ -30,11 +30,35 @@ function init() {
   addShip(shipStartingPosition)
 
   function addShip(position) {
-    console.log('ship>>', cells[position])
     cells[position].classList.add(shipClass)
   }
 
-  
+  function removeShip(position) {
+    cells[position].classList.remove(shipClass)
+  }
+
+  function controls(event) {
+    const key = event.keyCode
+
+    removeShip(shipCurrentPosition)
+    if (key === 37) {
+      shipCurrentPosition--
+      console.log('left', shipCurrentPosition)
+    } else if (key === 38) {
+      shipCurrentPosition -= width
+      console.log('up', shipCurrentPosition)
+    } else if (key === 39) {
+      shipCurrentPosition++
+      console.log('right', shipCurrentPosition)
+    } else if (key === 40) {
+      shipCurrentPosition += width
+      console.log('down',shipCurrentPosition)
+    } else {
+      console.log('invalid key')
+    }
+    addShip(shipCurrentPosition)
+  }
+
 
   document.addEventListener('keyup', controls)
 }
