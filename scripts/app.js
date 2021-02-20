b497c5b5c4939dfde8ad94343ab437fb7cf8fcb4
hamzaaMB <hamzambutt1@gmail.com>
2021-02-20 19:04:08 +0000

    grid created

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,29 @@ function init() {
   
   console.log('content loaded')
 
+  const grid = document.querySelector('.grid')
+  const width = 10
+  const cellCount = width * width
+  const cells = []
 
 
+  const ship = document.querySelector('.ship')
 
 
+//grid
 
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.innerText = i
+      grid.appendChild(cell)
+      cells.push(cell)
+      console.log('CELL>>', cell)
+    }
+
+  }
+
+  createGrid()
 
 
 }
