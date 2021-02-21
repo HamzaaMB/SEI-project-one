f530df29d74eb21312c7e0d4e0a512e1259d9f45
hamzaaMB <hamzambutt1@gmail.com>
2021-02-21 21:14:52 +0000

    added rocket

--- scripts/app.js
+++ scripts/app.js
@@ -12,8 +12,9 @@ function init() {
   let shipCurrentPosition = 104
   
   const alienClass = 'alien'
-  let alienStartingPosition = 0
+  let alienStartingPosition = 16
 
+  const laserClass = 'laser'
   
 
 
@@ -51,25 +52,42 @@ function init() {
       shipCurrentPosition--
       console.log('left', shipCurrentPosition)
     } else if (key === 38) {
-      console.log('invalid key', shipCurrentPosition)
+      // console.log('invalid key', shipCurrentPosition)
     } else if (key === 39 && shipCurrentPosition % width !== width - 1 ) {
       shipCurrentPosition++
-      console.log('right', shipCurrentPosition)
+      // console.log('right', shipCurrentPosition)
     } else if (key === 40 && shipCurrentPosition + width <= width * height - 1) {
       shipCurrentPosition += width
-      console.log('down',shipCurrentPosition)
+      // console.log('down',shipCurrentPosition)
     } else {
-      console.log('invalid key')
+      // console.log('invalid key')
     }
     addShip(shipCurrentPosition)
   }
 
   function alienControls() {
-    console.log('alien alive')
+    // console.log('alien alive')
+    cells[alienStartingPosition].classList.add(alienClass)
+  }
+  alienControls()
+
+
+  function laserControls(event) {
+    console.log('laser go')
+    const key = event.keyCode
+
+    if (key === 32) {
+      console.log('spacebar is being pressedr>>')
+      cells[93].classList.add(laserClass)
+
+    }
   }
 
   /*EVENT LISTENERS*/
 
+  document.addEventListener('keyup', laserControls)
   document.addEventListener('keydown', shipControls)
 }
+
+
 window.addEventListener('DOMContentLoaded', init)
